2288ea7b97d589610ab2e2b4f1e62adbc9f07b4e
Vincent H. Perricone <vhp@lavabit.com>
2011-04-07 23:48:03 -0400

    Enhanced formatting and added conv=fsync

--- bestblock.py
+++ bestblock.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 #
-#   Author: Vincent Perricone <vhp@buffalo.edu>
+#   Author: Vincent Perricone <vhp lavabit com>
 #   Date: 4/6/2011
-#   Title: Best-Block
-#   Description: Best-Block is a simple script used to find your systems
+#   Title: Best Block - bestblock.py
+#   Description: Best Block is a simple script used to find your systems
 #               most efficient blocking factor. Compares speed when data is
 #               readily available as well as when it is not.
 #   License: Released under "Simplified BSD License" see LICENSE file
 #
 
 import os
-import decimal
 import subprocess
 from operator import itemgetter
+from decimal import Decimal
 
 FILENAME = 'temp.file'
 RANDFILE = 'rand.file'
@@ -24,16 +24,22 @@ GB = 1024 * 1024 * 1024
 blocksizes = [512, 1024, 4096, 8192, 262144, 524288, 1048576, 16777216, \
                33554432, 67108864, 268435456,  536870912, 1073741824]
 
-def removefiles():
-    """remove all temporary files, specifically FILENAME, RANDFILE """
-    ret = subprocess.call(['rm','-f', FILENAME, RANDFILE])
-    if ret == 0:
+def remove_files():
+    """ remove all temporary files """
+    return_value = subprocess.call(['rm','-f', FILENAME, RANDFILE])
+    sync()
+    if return_value == 0:
         return 1
     else:
         return 0    
 
-def blockprint(blocksize):
-    """ return formatted string of human readable block size unit like 1G """
+def sync():
+    """ Flush filesystem buffers. """
+    sync_pipe = subprocess.Popen(['sync'])
+    sync_pipe.wait()
+
+def pretty_blocks(blocksize):
+    """ Return formatted string of human readable block size unit like 1G """
     if blocksize % GB == 0:
         return '{0}G'.format(str(blocksize / GB))
     elif blocksize % MB == 0:
@@ -43,69 +49,77 @@ def blockprint(blocksize):
     else:
         return blocksize
 
-def tableprint(results, test):
-    """ print tabulated results to user """
-    count = 1
+def print_table(results, table_title):
+    """ Print tabulated results to user """
     if results:
-        print('\n\t\t'+ test)
+        print('\n\t\t'+ table_title)
         print('----------------------------------------------------------')
         print(' #  Block size\tEasy Unit\tTime\t\tThroughput')
-        for (blocksize, time, throughput) in results[0:3]:
-            print( str(count).rjust(2)+'  '+ str(blocksize).ljust(14)+' '+\
-              str(blockprint(blocksize)).ljust(4)+'\t\t'+str(time).ljust(7)+'s \t'\
-                    + str(throughput).rjust(4)+ ' MB/s')
-            count = count + 1
+        for index, (bsize, time, throughput) in enumerate(results[0:3],start=1):
+            print('{0} {1} {2}\t\t{3}s \t {4} MB/s'.format(
+                                        str(index).rjust(2),
+                                        str(bsize).ljust(14),
+                                        str(pretty_blocks(bsize)).ljust(4),
+                                        str(time).ljust(7),
+                                        str(throughput).rjust(4)))
     else:
-        print('Error: Empty list received upon table printing')
+        print('Error: Empty list of results received upon table printing')
 
 def quick_data_access():
     """ Evaluate block size when information is readily available """
-    quickaccess = []
-    for element in reversed(blocksizes):
-        pipe1 =  subprocess.Popen(['dd', 'if=/dev/zero' , 'of=' + FILENAME, \
-            'bs=' + str(element), 'count=' + str(GB/element)], \
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        pipe1.wait()
-        for line in pipe1.stderr:
+    results_list = []
+    for blocksize in reversed(blocksizes):
+        dd_subprocess = (subprocess.Popen(['dd', 'if=/dev/zero',
+                        'of=' + FILENAME,
+                        'bs=' + str(blocksize), 
+                        'count=' + str(GB / blocksize), 'conv=fsync'],
+                        stdout=subprocess.PIPE, stderr=subprocess.PIPE))
+        dd_subprocess.wait()
+        for line in dd_subprocess.stderr:
             if 'bytes' in line and 'copied' in line:
-                parted = line.partition(', ')[2].partition(', ')
-                quickaccess.append([int(element), decimal.Decimal(parted[0].rstrip(' s')),\
-                     decimal.Decimal(parted[2].rstrip('\n').rstrip('  MB/s'))])
-    removefiles()
-    tableprint(sorted(quickaccess, key=itemgetter(1)), 'Quick Access to Data')
+                dd_data = line.partition(', ')[2].partition(', ')
+                (results_list.append([int(blocksize),
+                    Decimal(dd_data[0].rstrip(' s')),
+                    Decimal(dd_data[2].rstrip('\n').rstrip('  MB/s'))]))
+    remove_files()
+    print_table(sorted(results_list, key=itemgetter(1)), 'Quick Access to Data')
 
-def make_rand_file():
+def create_random_file():
     """ Create file of size 1GB consistent of pseudo random data """
-    ret = subprocess.call(['dd','if=/dev/urandom','of='+RANDFILE,'bs=4096','count=262144'], stderr=subprocess.PIPE)
-    if ret == 0 and os.path.isfile('rand.file'):
+    ret = (subprocess.call(['dd', 'if=/dev/urandom', 'of=' + RANDFILE,
+                            'bs=4096', 'count=262144'], stderr=subprocess.PIPE))
+    if ret == 0 and os.path.isfile(RANDFILE):
         return 1
     else:
         return 0
 
 def same_disk_file_access():
     """ Evaluate block size when data is I/O dependent """
-    samediskaccess = []
-    if make_rand_file() == 1:
-        for element in reversed(blocksizes):
-            pipe1 =  subprocess.Popen(['dd', 'if='+RANDFILE , 'of='+FILENAME, \
-            'bs='+str(element), 'count='+str(GB/element)], \
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            pipe1.wait()
-            for line in pipe1.stderr:
+    results_list = []
+    if create_random_file() == 1:
+        for blocksize in reversed(blocksizes):
+            dd_subprocess = (subprocess.Popen(['dd', 'if=' + "/dev/urandom" , 
+                            'of=' + FILENAME, 'bs=' + str(blocksize),
+                            'count=' + str(GB / blocksize), 'conv=fsync'],
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE))
+            dd_subprocess.wait()
+            for line in dd_subprocess.stderr:
                 if 'bytes' in line and 'copied' in line:
-                    parted = line.partition(', ')[2].partition(', ')
-                    samediskaccess.append([int(element), decimal.Decimal(parted[0].rstrip(' s')),\
-                     decimal.Decimal(parted[2].rstrip('\n').rstrip('  MB/s'))])
+                    dd_data = line.partition(', ')[2].partition(', ')
+                    (results_list.append([int(blocksize), 
+                        Decimal(dd_data[0].rstrip(' s')),
+                        Decimal(dd_data[2].rstrip('\n').rstrip('  MB/s'))]))
     else:
-        print('Error: Random Test file ' + RANDFILE + ' does not exist')
-    tableprint(sorted(samediskaccess, key=itemgetter(1)), 'Same Disk File Access')
+        print('Error: Random Test file {0} does not exist'.format(RANDFILE))
+    print_table(sorted(results_list, key=itemgetter(1)), 'Same Disk File Access')
 
 if __name__ == '__main__':
-    print('Best-Block is now creating temp files and measuring performance.')
+    print('Best Block is now measuring your systems performance.')
     print('Please be patient as this may take some time.')
-    if removefiles():
+    if remove_files():
+        sync()
         quick_data_access()
         same_disk_file_access()
     else:
         print('Error: Could not properly erase temp files.')
-    removefiles()
+    remove_files()
